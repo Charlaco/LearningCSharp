2124e7a3db3a7703e5a4c6b554fc2b9f6bea5560
cacharelo <cacharelo@DESKTOP-OOP72TI>
2021-12-26 18:49:00 +0100

    Agregar archivos de proyecto.

---
+++ Car/Car.cs
@@ -0,0 +1,68 @@
+public class Car
+{
+    // Miembros de la clase
+    private string color;
+    private string brand;
+    private string model;
+    private int doors;
+    private string fuel;
+
+   
+
+    // Constructor con parámetros
+    public Car(string color, string brand, string model, int doors, string fuel)
+    {
+        this.color = color;
+        this.brand = brand;
+        this.model = model;
+        this.doors = doors;
+        this.fuel = fuel;
+    }
+
+    // Getters and setters
+
+    public string getColor()
+    {
+        return this.color;
+    }
+    public void setColor(string color)
+    {
+        this.color = color;
+    }
+
+    public string getBrand()
+    {
+        return this.brand;
+    }
+    public void setBrand(string brand)
+    {
+        this.brand = brand;
+    }
+
+    public string getModel()
+    {
+        return this.model;
+    }
+    public void setModel(string model)
+    {
+        this.model = model;
+    }
+
+    public int getDoors()
+    {
+        return this.doors;
+    }
+    public void setDoors(int doors)
+    {
+        this.doors = doors;
+    }
+
+    public string getFuel()
+    {
+        return this.fuel;
+    }
+    public void setFuel(string fuel)
+    {
+        this.fuel = fuel;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// using Car;
+using System;
+
+
+public class CarFactory
+{
+    public static void Main()
+    {
+        // Instanciamos un nuevo coche 
+        Car car = new Car("rojo", "seat", "panda", 3, "gasoil");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Marca: " + car.getBrand());
+        Console.WriteLine("Modelo: " + car.getModel());
+        Console.WriteLine("Puertas: " + car.getDoors());
+        Console.WriteLine("Combustible: " + car.getFuel());
+        Console.WriteLine("Color: " + car.getColor());
+    }
+}
